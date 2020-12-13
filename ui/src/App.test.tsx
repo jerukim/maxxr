5cea0120512fe96a4635bf5a230359cc277dc24d
jerukim <mikurej95@gmail.com>
2020-12-13 07:37:02 -0500

    Update App.test.tsx

--- ui/src/App.test.tsx
+++ ui/src/App.test.tsx
@@ -3,9 +3,7 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 test('renders navbar', () => {
-  const { container } = render(<App />)
-
-  expect(container.firstChild?.firstChild?.tagName).toEqual('HEADER')
+  render(<App />)
 
   const logoElement = screen.getByText('Maxxr')
   expect(logoElement).toBeInTheDocument()
