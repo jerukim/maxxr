f8aa1ace3181c818e40df21f544d2f6937444fca
jerukim <mikurej95@gmail.com>
2020-12-13 03:19:05 -0500

    Create Home.test.tsx

---
+++ ui/src/Home.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from './Home';
+
+xtest('renders learn react link', () => {
+    // render(<Home />);
+    // const linkElement = screen.getByText(/learn react/i);
+    // expect(linkElement).toBeInTheDocument();
+});
+
+
