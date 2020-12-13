dc6919f23074874c33e769dc0ca8f5f653d16560
jerukim <mikurej95@gmail.com>
2020-12-13 03:18:36 -0500

    add a basic router function that renders content based on view

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { UserData } from './types'
+
 import Home from './Home'
+import Auth from './Auth'
+import Nav from './Nav'
 
 import './App.css'
-import "antd/dist/antd.css"
+import 'antd/dist/antd.css'
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState<UserData | null>(null) // <UserData>
+  const [view, setView] = useState<string>('home')
+
+  const renderContent = (view: string) => {
+    switch (view) {
+      case 'home':
+        return <Home />
+      case 'signup':
+        return <Auth view={view} setUser={setUser} />
+      case 'signup':
+        return <Auth view={view} setUser={setUser} />
+      // case 'dashboard':
+      //   return <Dashboard />
+      // case 'profile':
+      //   return <Profile />
+      default:
+        return <Home />
+    }
+  }
+
   return (
-    <div className="App">
-      <Home />
+    <div className='App'>
+      <Nav setView={setView} />
+      {renderContent(view)}
     </div>
   )
 }
