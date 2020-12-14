8430bdfff6ffa6c2aa1ea6c1e8c68b2a58dc317a
jerukim <mikurej95@gmail.com>
2020-12-13 23:00:33 -0500

    update error handler with correct arguments

--- api/src/middleware/error.ts
+++ api/src/middleware/error.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { CustomError } from '../models/index'
 
-export const errorHandler = (error: CustomError, _: Request, response: Response) => {
+export const errorHandler = (error: CustomError, _: Request, response: Response, next: NextFunction) => {
     const { statusCode, message, detail } = error
 
     response
