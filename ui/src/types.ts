bf41dbc1de4c5994c0318f3f37471adf58a1765f
jerukim <mikurej95@gmail.com>
2020-12-15 04:33:52 -0500

    Update types.ts

--- ui/src/types.ts
+++ ui/src/types.ts
@@ -1,13 +1,22 @@
+export * from './store'
+export type { AuthMethod } from './auth/Auth'
+
+export interface APIError {
+    code: number,
+    message: string,
+    detail: string
+}
+
 export interface UserAuthInput {
     username: string,
     password: string,
 }
 
 export interface UserData {
-    id: number,
-    username: string,
-    password?: string,
-    token: string,
+    id?: number,
+    username?: string,
+    token?: string,
+    error?: APIError,
     created_at?: string,
     updated_at?: string,
 }
