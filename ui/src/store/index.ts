2bfb8d7e4a11186132199f8d114f92e9c3d1b31d
jerukim <mikurej95@gmail.com>
2020-12-16 00:02:06 -0500

    persist user data in local storage

--- ui/src/store/index.ts
+++ ui/src/store/index.ts
@@ -1,14 +1,27 @@
 import { configureStore, combineReducers, Action } from '@reduxjs/toolkit'
 import { ThunkAction } from 'redux-thunk'
+import { loadState, saveState } from '../localStorage'
+import throttle from 'lodash/throttle'
 import userReducer from './user'
 
 const rootReducer = combineReducers({
     user: userReducer
 })
 
+const persistedState = loadState()
+
 const store = configureStore({
-    reducer: rootReducer
+    reducer: rootReducer,
+    preloadedState: persistedState
 })
+
+const persistUser = () => {
+    const { user } = store.getState()
+
+    saveState({ user })
+}
+
+store.subscribe(throttle(persistUser, 1000))
 
 export default store
 
