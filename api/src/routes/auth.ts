25443832514c9e27136a43c390eb0f8eb39b7d58
jerukim <mikurej95@gmail.com>
2020-12-16 02:58:12 -0500

    use authentication middleware for /auth/signout route

--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express'
+import { isAuthenticated } from '../middleware'
 import { User } from '../controllers/index'
 
 export const authRouter = Router()
 
 authRouter.post('/signup', User.signup)
 authRouter.post('/signin', User.signin)
+
+authRouter.use(isAuthenticated)
+
+authRouter.post('/signout', User.signout)
