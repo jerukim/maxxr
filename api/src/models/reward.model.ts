1ebb261f2a5c5c17d8b582f54e9043e28850f70e
jerukim <mikurej95@gmail.com>
2020-12-19 01:15:41 -0500

    rewards model find all function filters out inactive rewards

--- api/src/models/reward.model.ts
+++ api/src/models/reward.model.ts
@@ -1,12 +1,18 @@
 import db from '../db'
 
 class Reward {
-    async findAllByCardId(cardId: number) {
+    async findAll(cardId: number, userId: number) {
         const rewards = await db('reward')
+            .select('reward.rate', 'reward.active_quarter', 'reward.expires_at', 'reward.expires_in', 'category.name as category', 'subcategory.name as subcategory')
             .leftJoin('category', 'reward.category_id', '=', 'category.id')
             .leftJoin('subcategory', 'reward.subcategory_id', '=', 'subcategory.id')
-            .select('reward.rate', 'reward.active_quarter', 'reward.expires_at', 'reward.expires_in', 'category.name as category', 'subcategory.name as subcategory')
-            .where({ card_id: cardId })
+            .join('card', 'reward.card_id', '=', 'card.id')
+            .join('user_card', 'user_card.card_id', '=', 'card.id')
+            .where({ 'reward.card_id': cardId, 'user_card.user_id': userId })
+            .whereRaw('(reward.active_quarter ISNULL OR reward.active_quarter::INTEGER = EXTRACT(QUARTER FROM CURRENT_DATE)::INTEGER)')
+            .whereRaw('(reward.expires_at ISNULL OR reward.expires_at >= CURRENT_DATE)')
+            .whereRaw('(reward.expires_in ISNULL OR user_card.approved_on + reward.expires_in::INTERVAL <= CURRENT_DATE)')
+            .orderBy('reward.rate')
 
         return rewards
     }
