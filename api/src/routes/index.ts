a403689570bfa67b489782c7797ae6ee15501949
jerukim <mikurej95@gmail.com>
2020-12-19 01:15:46 -0500

    Update index.ts

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,3 +1,4 @@
 export { default as auth } from './auth'
 export { default as category } from './category'
 export { default as wallet } from './wallet'
+export { default as recommendation } from './recommendation'
